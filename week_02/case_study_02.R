bced64366263854723ed7178c3c3aba2fe229b8b
Andrea Harder <amharder@buffalo.edu>
2024-10-31 12:00:54 -0400

    Case study 2

---
+++ week_02/case_study_02.R
@@ -0,0 +1,31 @@
+#Define the link to the data
+library(tidyverse)
+
+#define the link to the data - you can try this in your browser too.  Note that the URL ends in .txt.
+dataurl="https://data.giss.nasa.gov/tmp/gistemp/STATIONS_v4/tmp_USW00014733_14_0_1/station.csv"
+
+#the next line tells the NASA site to create the temporary file
+httr::GET("https://data.giss.nasa.gov/cgi-bin/gistemp/stdata_show_v4.cgi?id=USW00014733&ds=14&dt=1")
+
+temp=read_csv(dataurl, 
+              na="999.90", # tell R that 999.90 means missing in this dataset
+              skip=1, # we will use our own column names as below so we'll skip the first row
+              col_names = c("YEAR","JAN","FEB","MAR", # define column names 
+                            "APR","MAY","JUN","JUL",  
+                            "AUG","SEP","OCT","NOV",  
+                            "DEC","DJF","MAM","JJA",  
+                            "SON","metANN"))
+
+#Explore
+View(temp)
+summary(temp)
+
+#Graph annual mean temperature JJA 
+ggplot(temp,aes(YEAR,JJA))+
+  geom_point() +
+  geom_line() +
+  geom_smooth() +
+  xlab("Year") + ylab("Mean Summer Temperature (C)")+
+  ggtitle("Mean Summer Temperatures in Buffalo, NY")
+
+ggsave 
